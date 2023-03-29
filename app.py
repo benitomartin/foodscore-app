2118f3fdcd18594b0e3067d37f2bdb6ea27c63c6
benito martin <bmartinc80@gmail.com>
2023-03-29 12:35:13 +0200

    Baseline App

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 import streamlit as st
 import requests
-import pandas as pd
-import h5py
-from keras.models import load_model
-import cv2
-import matplotlib.pyplot as plt
 from streamlit_lottie import st_lottie
-
 from PIL import Image
-# from io import BytesIO
 import plotly.express as px
-from keras.utils import load_img, img_to_array
-import numpy as np
-from df import get_df
+from streamlit_cropper import st_cropper
+
+
+
 
 st.set_page_config(page_title="FoodScore", page_icon=":food:", layout="wide")
 
 # For testing purpose. Must be change once API is ready
-model = load_model("model_weights/model_vgg16_cl.h5", compile = False)
+# model = load_model("model_weights/model_vgg16_cl.h5", compile = False)
 # response = requests.get(uploaded_file, params = params).json()
 # st.image(model.predict(uploaded_file))
 
 
-# Nutrition Dataset Upload
-nutr_df = get_df("nutrition101.csv")
-subset = ['protein', 'calcium', 'fat', 'carbohydrates', 'vitamins']
+
 
 # Funciton for styling the Nutrition Dataset
 def make_pretty(styler):
@@ -99,72 +91,145 @@ with st.container():
 with st.container():
     if uploaded_file is not None:
 
-        col1, col2 = st.columns([1,1])
+        #col1, col2 = st.columns([1,1])
 
-        image = Image.open(uploaded_file)
-
-        with col1:
-            st.markdown("---")
-            image = Image.open(uploaded_file)
-            fig = px.imshow(image)
-            fig.update_layout(width=500, height=600, margin=dict(l=1, r=1, b=1, t=1))
-            fig.update_layout(hovermode=False)
-            fig.update_xaxes(showticklabels=False)
-            fig.update_yaxes(showticklabels=False)
-            st.plotly_chart(fig, use_container_width=True)
+        st.image(uploaded_file)
 
 
-        with col2:
+#with col1:
+        st.markdown("---")
 
-            st.markdown("---")
+        # Upload an image and set some options for demo purposes
+        # st.header("Cropper Demo")
+
+        # realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
+        # box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
+        # aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
+        # aspect_dict = {
+        #     "1:1": (1, 1),
+        #     "16:9": (16, 9),
+        #     "4:3": (4, 3),
+        #     "2:3": (2, 3),
+        #     "Free": None
+        # }
+        # aspect_ratio = aspect_dict[aspect_choice]
+
+        # if uploaded_file:
+        #     img = Image.open(uploaded_file)
+        #     fig = px.imshow(img)
+        #     fig.update_layout(width=500, height=600, margin=dict(l=1, r=1, b=1, t=1))
+        #     fig.update_layout(hovermode=False)
+        #     fig.update_xaxes(showticklabels=False)
+        #     fig.update_yaxes(showticklabels=False)
+        #     st.plotly_chart(fig, use_container_width=True)
+
+        #     if not realtime_update:
+        #         st.write("Double click to save crop")
+
+        #     # Get a cropped image from the frontend
+        #     cropped_img = st_cropper(img, realtime_update=realtime_update, box_color=box_color,
+        #                                 aspect_ratio=aspect_ratio)
+
+        #     # Manipulate cropped image at will
+        #     st.write("Preview")
+        #     _ = cropped_img.thumbnail((300,300))
+        #     st.image(cropped_img)
+
+
+        # image = Image.open(uploaded_file)
+        # fig = px.imshow(image)
+        # fig.update_layout(width=500, height=600, margin=dict(l=1, r=1, b=1, t=1))
+        # fig.update_layout(hovermode=False)
+        # fig.update_xaxes(showticklabels=False)
+        # fig.update_yaxes(showticklabels=False)
+        # st.plotly_chart(fig, use_container_width=True)
+
+
+        #with col2:
+
+        st.markdown("---")
+        url = "https://foodscore-j5kdnfjkoa-ew.a.run.app/upload_image"
+
+        url = "http://localhost:8000/upload_image"
+
+        #dict_food = requests.post(url,files={'img':open('test_fotos/1.jpg','rb')}).json()
+
+
+
+        dict_food = requests.post(url,files={'img':uploaded_file.getvalue()}).json()
+
+
+
+
+        st.write(f"Is it {dict_food['name']['0']}?")
+
+        st.write(f"Or is it {dict_food['name']['1']}, {dict_food['name']['2']},{dict_food['name']['3']} or {dict_food['name']['4']}?")
+
+
+
+            #st.write(f"Your food has been categorized as {dict_food['name']['0']}")
+
+
+
+            #st.write('<p style="font-size:26px; color:white;">Your food has been categorized as Rice</p>',unsafe_allow_html=True)
+
+
+
+
+            # st.dataframe(df)
+
+            #res = requests.post(url, files={'img': uploaded_file})
+
 
             # Adding label to the Foto
-            image = Image.open(uploaded_file)
-            fig = px.imshow(image)
-            fig.update_layout(width=500, height=600, margin=dict(l=1, r=1, b=1, t=1))
-            fig.update_layout(hovermode=False)
-            fig.add_annotation(text="Is it Rice?", x=0.5, y=0.9, xref="paper", yref="paper", showarrow=False,
-                                font_size=40, font_color='Green', bgcolor="white")
-            fig.update_xaxes(showticklabels=False)
-            fig.update_yaxes(showticklabels=False)
-            st.plotly_chart(fig, use_container_width=True)
+            # image = Image.open(uploaded_file)
+            # fig = px.imshow(image)
+            # fig.update_layout(width=500, height=600, margin=dict(l=1, r=1, b=1, t=1))
+            # fig.update_layout(hovermode=False)
+            # fig.add_annotation(text="Is it Rice?", x=0.5, y=0.9, xref="paper", yref="paper", showarrow=False,
+            #                     font_size=40, font_color='Green', bgcolor="white")
+            # fig.update_xaxes(showticklabels=False)
+            # fig.update_yaxes(showticklabels=False)
+            # st.plotly_chart(fig, use_container_width=True)
 
 
 # Nutrition Container
-with st.container():
-    if uploaded_file is not None:
+# with st.container():
+#     if uploaded_file is not None:
 
-        col3, col4 = st.columns([1,1])
+#         col3, col4 = st.columns([1,1])
 
 
-        with col3:
-            st.write('<p style="font-size:26px; color:white;">Your food has been categorized as Rice</p>',unsafe_allow_html=True)
+#         with col3:
 
-            # Testing model weights
-            image_pred = load_img(uploaded_file, target_size=(224, 224))
-            image_pred = img_to_array(image_pred) / 255.0
-            image_pred_exp = np.expand_dims(image_pred, axis=0)
+#             # Testing model weights
+#             # image_pred = load_img(uploaded_file, target_size=(224, 224))
+#             # image_pred = img_to_array(image_pred) / 255.0
+#             # image_pred_exp = np.expand_dims(image_pred, axis=0)
 
-            labelPreds = model.predict(image_pred_exp)
-            st.write(labelPreds)
-            st.write(f'The label highest probability is: {labelPreds[0][0]:.3f}')
-            st.write(f'The label highest probability is: {labelPreds[0][1]:.3f}')
+#             # labelPreds = model.predict(image_pred_exp)
+#             # st.write(labelPreds)
+#             # st.write(f'The label highest probability is: {labelPreds[0][0]:.3f}')
+#             # st.write(f'The label highest probability is: {labelPreds[0][1]:.3f}')
 
-        # Option whole dataset below one image
-        with col4:
+#         # Option whole dataset below one image
+#         with col4:
 
             # Show a Table with the nutrition facts. To be fine tuned
-            st.write('<p style="font-size:26px; color:white;">Here you can find your nutrition Data (min/max)</p>', unsafe_allow_html=True)
-            st.dataframe(nutr_df.style.pipe(make_pretty), use_container_width=True)
+            # st.write('<p style="font-size:26px; color:white;">Here you can find your nutrition Data (min/max)</p>', unsafe_allow_html=True)
+            # st.dataframe(nutr_df.style.pipe(make_pretty), use_container_width=True)
 
 
 
 # Option whole dataset below both images
-st.write(nutr_df['name'][0])
+# st.write(nutr_df['name'][0])
 
 
 st.write('<p style="font-size:26px; color:white;">Here you can find your nutrition Data (min/max)</p>', unsafe_allow_html=True)
-st.dataframe(nutr_df.style.pipe(make_pretty).format(formatter="{:.2f}", subset=subset), use_container_width=True)
+
+
+st.dataframe(dict_food)
+
 
 
 
